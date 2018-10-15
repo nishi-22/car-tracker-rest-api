79a83b31f2a3a229d6cb12c71b4d703f29b8f433
Nishi Lad <nishilad2208@gmail.com>
2018-10-14 21:22:40 -0500

    updated spring application

--- src/main/java/com/nishi/cartracker/cartrackerrestapi/CarTrackerRestApiApplication.java
+++ src/main/java/com/nishi/cartracker/cartrackerrestapi/CarTrackerRestApiApplication.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class CarTrackerRestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarTrackerRestApiApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String hello() {
-		return "Hello Spring Boot!";
 	}
 }
